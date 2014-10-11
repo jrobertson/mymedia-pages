7578ccdc0088d483dcc594b708c92714747f1ecd
Your Name <you@example.com>
2014-10-11 08:28:37 +0100

    1st commit

---
+++ lib/mymedia-pages.rb
@@ -0,0 +1,92 @@
+#!/usr/bin/env ruby
+
+# file: mymedia-pages.rb
+
+
+require 'mymedia-blogbase'
+
+
+class MyMediaPages < MyMediaBlogBase
+
+  def initialize(config: nil)
+    
+    super(media_type: 'pages', public_type: @public_type='pages', ext: '.(html|md)', config: config)
+    @media_src = "%s/media/pages" % @home
+  end
+  
+  def copy_publish_to_be_removed(src_path, raw_msg='')
+    msg = super(src_path, raw_msg)
+  end
+  
+  def copy_publish(filename, raw_msg='')
+    
+    @filename = filename
+    src_path = File.join(@media_src, filename)
+
+    if File.basename(src_path)[/[pw]\d{6}T\d{4}\.(?:html)/] then      
+      return file_publish(src_path, raw_msg)
+    end
+    
+    file_publish(src_path, raw_msg) do |destination, raw_destination|
+      
+      #FileUtils.cp src_path, destination
+      #FileUtils.cp src_path, raw_destination   
+      
+      if File.extname(src_path) == '.md' then      
+        
+        raw_dest_xml = raw_destination.sub(/html$/,'xml')          
+        md_destination = raw_destination.sub(/html$/,'md')
+        FileUtils.cp src_path, md_destination
+        
+        doc = xml(File.open(src_path, 'r').read, 
+                        md_destination[/\/r\/#{@public_type}.*/], filename)          
+        
+        modify_xml(doc, raw_dest_xml)
+
+        basic_xsl = File.read "#{@home}/r/xsl/#{@public_type}.xsl"
+                
+        File.write raw_destination, 
+          Nokogiri::XSLT(basic_xsl)\
+            .transform(Nokogiri::XML(File.read raw_dest_xml))
+
+        xsl = File.read "#{@home}/#{@www}/xsl/#{@public_type}.xsl"
+
+        File.write destination, 
+           Nokogiri::XSLT(xsl).transform(Nokogiri::XML(File.read raw_dest_xml))
+        else
+          FileUtils.cp src_path, destination
+          FileUtils.cp src_path, raw_destination   
+      end
+      
+
+      if not File.basename(src_path)[/[pw]\d{6}T\d{4}\.(?:html|md)/] then
+        html_filename = File.basename(src_path).sub(/md$/,'html')
+
+        FileUtils.cp destination, @home + "/#{@public_type}/" + html_filename
+        html_filepath = @home + "/#{@public_type}/static.xml"          
+        target_url = [@website, @public_type, html_filename].join('/')
+        publish_dynarex(html_filepath, {title: html_filename, url: target_url })          
+        tags = doc.root.xpath('summary/tags/tag/text()')
+
+        raw_msg = "%s %s" % [doc.root.text('summary/title'), 
+               tags.map {|x| "#%s" % x }.join]
+
+      end
+
+      [raw_msg, target_url]
+    end    
+
+  end
+  
+  private
+  
+  def modify_xml(doc, filepath)
+    
+    raw_msg = microblog_title2(doc)
+    doc.instructions.push %w(xml-stylesheet title='XSL_formatting' type='text/xsl') + ["href='#{@website}/r/xsl/#{@public_type}.xsl'"]
+    doc = yield(doc) if block_given?
+    File.write filepath, doc.xml(pretty: true)    
+  end
+     
+  
+end
---
+++ mymedia-pages.gemspec
@@ -0,0 +1,13 @@
+Gem::Specification.new do |s|
+  s.name = 'mymedia-pages'
+  s.version = '0.1.0'
+  s.summary = 'A MyMedia gem to publish a basic web page'
+  s.authors = ['James Robertson']
+  s.files = Dir['lib/**/*.rb']
+  s.add_runtime_dependency('mymedia-blogbase', '~> 0.1', '>=0.1.0')
+  s.signing_key = '../privatekeys/mymedia-pages.pem'
+  s.cert_chain  = ['gem-public_cert.pem']
+  s.license = 'MIT'
+  s.email = 'james@r0bertson.co.uk'
+  s.homepage = 'https://github.com/jrobertson/mymedia-pages'
+end
